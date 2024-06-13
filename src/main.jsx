a6ba9478ee66aae82295a9fa934352399a3b86bb
BunnyKarru <wtimepass6@gmail.com>
2024-06-13 20:46:14 +0530

    fifth

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
+import Page from './components/Page.jsx';
+import SearchResults from './components/SearchResults.jsx';
+import NoResults from './components/NoResults.jsx';
+import FoodItem from './components/FoodItem.jsx';
 
-import './index.css'
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Layout from './components/Layout.jsx'
-import Page from './components/Page.jsx'
-import SearchResults from './components/SearchResults.jsx'
-import NoResults from './components/NoResults.jsx'
-import FoodItem from './components/FoodItem.jsx'
-
-const router = createBrowserRouter(
-  [
+const router = createBrowserRouter({
+  basename: '/vite-react-deploy/',  // Adjust this with your actual base name
+  routes: [
     {
-      path:'/',
-      element:<Layout/>,
-      children:[
+      path: '/',
+      element: <Layout />,
+      children: [
         {
-          path:"",
-          element:<Page/>
+          path: "",
+          element: <Page />
         },
         {
-          path:"/foodItem",
-          element:<FoodItem/>
+          path: "/foodItem",
+          element: <FoodItem />
         },
         {
-          path:'/searchResults',
-          element:<SearchResults/>
+          path: '/searchResults',
+          element: <SearchResults />
         },
         {
-          path:'/noresults',
-          element:<NoResults/>
+          path: '/noresults',
+          element: <NoResults />
         }
       ]
     }
   ]
-)
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <RouterProvider router={router}/>
-    
-  </React.StrictMode>,
-)
+    <router.RouterProvider router={router}>
+      <router.RouterView />
+    </router.RouterProvider>
+  </React.StrictMode>
+);
